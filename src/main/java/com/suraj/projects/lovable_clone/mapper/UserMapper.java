ed127fde4cf5ce90cbacb6ebfbd164826152eb6c
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-06-18 12:40:56 +0530

    Add spring-boot-starter-security dependency in pom.xml; enhance BadRequestException with Lombok annotations; implement user signup logic in AuthServiceImpl with username validation and password encoding; add UserMapper method for SignupRequest conversion.

--- src/main/java/com/suraj/projects/lovable_clone/mapper/UserMapper.java
+++ src/main/java/com/suraj/projects/lovable_clone/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package com.suraj.projects.lovable_clone.mapper;
 
+import com.suraj.projects.lovable_clone.dto.auth.SignupRequest;
 import com.suraj.projects.lovable_clone.dto.auth.UserProfileResponse;
 import com.suraj.projects.lovable_clone.entity.User;
 import org.mapstruct.Mapper;
@@ -7,5 +8,6 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    User toEntity(SignupRequest request);
     UserProfileResponse toUserProfileResponse(User user);
 }
